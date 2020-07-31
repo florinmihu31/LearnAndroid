87b5a64e6324d44d03eb6e30b25fe548b533745c
florinmihu31 <53407990+florinmihu31@users.noreply.github.com>
2020-07-31 16:26:57 +0300

    Initial commit for Picker For Date project

---
+++ PickerForDate/app/src/main/java/com/example/pickerfordate/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pickerfordate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String dayString = Integer.toString(day);
+        String monthString = Integer.toString(month + 1);
+        String yearString = Integer.toString(year);
+        String dateMessage = (dayString + "/" + monthString + "/" + yearString);
+
+        Toast.makeText(this, getString(R.string.date_message) + dateMessage, Toast.LENGTH_SHORT).show();
+    }
+}
