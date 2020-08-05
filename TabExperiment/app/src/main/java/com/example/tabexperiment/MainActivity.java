fa509af1e5a169381924523cbeebe46f193ce638
florinmihu31 <53407990+florinmihu31@users.noreply.github.com>
2020-08-05 12:54:01 +0300

    Add tab fragments

--- TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
+++ TabExperiment/app/src/main/java/com/example/tabexperiment/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tabexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
